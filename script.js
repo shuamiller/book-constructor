96ebac3b97cb75192e19688c42da336fb3f014d7
Shua Miller <thejoshmill@gmail.com>
2020-11-02 14:19:21 -0700

    Finish functionality for creating basic book cards with correct text implementation. Create icon to be used as delete button with styling.

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let bookButton = document.getElementById('book-button');
 let body = document.querySelector('body');
-let cardsDiv = document.getElementById('cards-div');
 
-let myLibrary = [theSicknessUntoDeath];
+let cardsDiv = document.getElementById('cards-div');
+let addBox = document.createElement('div');
+let form = document.createElement('form');
+let submitDiv = document.createElement('div');
+let submitButton = document.createElement('input');
+let titleInput = document.createElement('input');
+let authorInput = document.createElement('input');
+let pagesInput = document.createElement('input');
+let yesRadio = document.createElement('input');
+let noRadio = document.createElement('input');
+let radios = document.getElementsByName('read-radio');
 
 let theSicknessUntoDeath = {
-    title = "The Sickness Unto Death",
-    author = "Soren Kierkegaard",
-    pages = 201,
-    readStatus = "Yes"
+    title: "The Sickness Unto Death",
+    author: "Soren Kierkegaard",
+    pages: 201,
+    readStatus: "Yes"
 }
+
+let myLibrary = [theSicknessUntoDeath];
 
 function Book(title, author, pages, readStatus) {
     this.title = title
@@ -25,11 +36,20 @@ function createOverlay() {
     let overlay = document.createElement('div');
     overlay.setAttribute('id', 'overlay');
     body.appendChild(overlay);
+    return overlay;
 }
 
 function createAddBox() {
-    let addBox = document.createElement('div');
+    
     addBox.setAttribute('id', 'add-box');
+    body.appendChild(addBox);
+    return addBox;
+}
+
+function createForm() {
+    
+    addBox.appendChild(form);
+    return form;
 }
 
 function createTitleComponent() {
@@ -41,12 +61,13 @@ function createTitleComponent() {
     titleLabel.setAttribute('for', 'title')
     titleLabel.setAttribute('id', 'title-label');
     titleLabel.setAttribute('class', 'input-label');
-    let titleInput = document.createElement('input');
+    
+    titleInput.required = true;
     titleInput.setAttribute('type', 'text');
     titleInput.setAttribute('id', 'title-input');
     titleInput.setAttribute('class', 'text-input');
     titleInput.setAttribute('name', 'title');
-    titleInput.setAttribute('required');
+
     titleDiv.appendChild(titleLabel);
     titleDiv.appendChild(titleInput);
     form.appendChild(titleDiv);
@@ -58,16 +79,17 @@ function createAuthorComponent() {
     authorDiv.setAttribute('id', 'author-div');
     authorDiv.setAttribute('class', 'input-div');
     let authorLabel = document.createElement('label');
-    authorLabel.textContent("Add a Book Author");
+    authorLabel.textContent = "Add a Book Author";
     authorLabel.setAttribute('for', 'author');
     authorLabel.setAttribute('id', 'author-label');
     authorLabel.setAttribute('class', 'input-label');
-    let authorInput = document.createElement('input');
+    
+    authorInput.required = true;
     authorInput.setAttribute('type', 'text');
     authorInput.setAttribute('id', 'author-input');
     authorInput.setAttribute('class', 'text-input');
     authorInput.setAttribute('name', 'author');
-    authorInput.setAttribute('required');
+    
     authorDiv.appendChild(authorLabel);
     authorDiv.appendChild(authorInput);
     form.appendChild(authorDiv);
@@ -79,20 +101,21 @@ function createPagesComponent() {
     pagesDiv.setAttribute('id', 'pages-div');
     pagesDiv.setAttribute('class', 'input-div');
     let pagesLabel = document.createElement('label');
-    pagesLabel.textContent("Add Page Count");
+    pagesLabel.textContent = "Add Page Count";
     pagesLabel.setAttribute('for', 'pages');
     pagesLabel.setAttribute('id', 'pages-label');
     pagesLabel.setAttribute('class', 'input-label');
-    let pagesInput = document.createElement('input');
+    
+    pagesInput.required = true;
     pagesInput.setAttribute('type', 'number');
     pagesInput.setAttribute('id', 'pages-input');
     pagesInput.setAttribute('class', 'text-input');
     pagesInput.setAttribute('name', 'pages');
-    pagesInput.setAttribute('required');
+    
     pagesDiv.appendChild(pagesLabel);
     pagesDiv.appendChild(pagesInput);
     form.appendChild(pagesDiv);
-    return pagesInput;
+
 }
 
 function createReadComponent() {
@@ -100,51 +123,55 @@ function createReadComponent() {
     readDiv.setAttribute('id', 'read-div');
     readDiv.setAttribute('class', 'input-div');
     let readLabel = document.createElement('label');
-    readLabel.textContent("Have You Read It?");
+    readLabel.textContent = "Have You Read It?";
     readLabel.setAttribute('for', 'read');
     readLabel.setAttribute('id', 'read-label');
     readLabel.setAttribute('class', 'input-label');
     let yesDiv = document.createElement('div');
     yesDiv.setAttribute('id', 'yes-div');
     yesDiv.setAttribute('class', 'radio-div');
-    let yesRadio = document.createElement('input');
+    
     yesRadio.setAttribute('type', 'radio');
     yesRadio.setAttribute('id', 'yes-radio');
     yesRadio.setAttribute('class', 'radio');
     yesRadio.setAttribute('name', 'read-radio');
-    yesRadio.setAttribute('value', 'yes')
+    yesRadio.setAttribute('value', 'Yes')
     let yesLabel = document.createElement('label');
     yesLabel.setAttribute('for', 'yes');
     yesLabel.textContent = "Yes";
+    yesDiv.appendChild(yesRadio);
+    yesDiv.appendChild(yesLabel);
     let noDiv = document.createElement('div');
     noDiv.setAttribute('id', 'no-div');
     noDiv.setAttribute('class', 'radio-div');
-    let noRadio = document.createElement('input');
+    
     noRadio.setAttribute('type', 'radio');
     noRadio.setAttribute('id', 'no-radio');
     noRadio.setAttribute('class', 'radio');
     noRadio.setAttribute('name', 'read-radio');
+    noRadio.setAttribute('value', 'No');
     let noLabel = document.createElement('label');
     noLabel.setAttribute('for', 'no');
     noLabel.textContent = "No";
+    noDiv.appendChild(noRadio);
+    noDiv.appendChild(noLabel);
     readDiv.appendChild(readLabel);
     readDiv.appendChild(yesDiv);
     readDiv.appendChild(noDiv);
     form.appendChild(readDiv);
-    let radios = document.getElementsByName('read-radio');
-    return radios;
+    
 }
 
 function createSubmitComponent() {
-    let submitDiv = document.createElement('div');
+    
     submitDiv.setAttribute('id', 'submit-div');
-    let submitButton = document.createElement('input');
+    
     submitButton.setAttribute('id', 'submit-button');
     submitButton.setAttribute('type', 'submit')
     submitButton.setAttribute('value', "Add Book");
     submitDiv.appendChild(submitButton);
     form.appendChild(submitDiv);
-    return submitButton;
+    
 }
 
 function checkRadio(radios) {
@@ -156,49 +183,65 @@ function checkRadio(radios) {
 }
 
 function createBookObject() {
-    let titleValue = titleInput.textContent;
-    let authorValue = authorInput.textContent;
-    let pagesValue = pagesInput.textContent;
+    let titleValue = titleInput.value;
+    let authorValue = authorInput.value;
+    let pagesValue = pagesInput.value;
     let readValue = checkRadio(radios);
     let newBook = new Book(titleValue, authorValue, pagesValue, readValue);
+    myLibrary.push(newBook);
     return newBook;
 }
 
-function createBookCard() {
+function createBookCard(book) {
     let bookCard = document.createElement('div');
     bookCard.setAttribute('class', 'book-card');
+    let headingDiv = document.createElement('div');
+    headingDiv.setAttribute('class', 'heading-div');
     let bookHeading = document.createElement('h2');
     bookHeading.setAttribute('class', 'book-heading');
-    bookHeading.textContent = newBook.title;
-    bookCard.appendChild(bookHeading);
+    bookHeading.textContent = `${book.title}`;
+    headingDiv.appendChild(bookHeading);
+    let deleteDiv = document.createElement('div');
+    deleteDiv.setAttribute('class', 'delete-div');
+    let deleteButton = document.createElement('i');
+    deleteButton.setAttribute('class', 'fas fa-trash-alt');
+    deleteDiv.appendChild(deleteButton);
+    headingDiv.appendChild(deleteDiv);
+    bookCard.appendChild(headingDiv);
     let bookAuthor = document.createElement('p');
     bookAuthor.setAttribute('class', 'book-author');
-    bookAuthor.textContent = `Author: ${newBook.author}`;
+    bookAuthor.textContent = `Author: ${book.author}`;
     bookCard.appendChild(bookAuthor);
     let bookPages = document.createElement('p');
     bookPages.setAttribute('class', 'book-pages');
-    bookPages.textContent = `Page Count: ${newBook.pages}`;
+    bookPages.textContent = `Page Count: ${book.pages}`;
     bookCard.appendChild(bookPages);
     let bookRead = document.createElement('p');
     bookRead.setAttribute('class', 'book-read');
-    bookRead.textContent = `Finished: ${newBook.readStatus}`
+    bookRead.textContent = `Finished: ${book.readStatus}`;
+    bookCard.appendChild(bookRead);
+    cardsDiv.appendChild(bookCard);
 }
 
-bookButton.addEventListener(function() {
+bookButton.addEventListener('click', function() {
     createOverlay();
     createAddBox();
-    let form = document.createElement('form');
+    createForm();
     createTitleComponent();
     createAuthorComponent();
     createPagesComponent();
     createReadComponent();
     createSubmitComponent();
-    submitButton.addEventListener(function() {
-        createBookObject();
+    submitButton.addEventListener('click', function() {
         
+        
+        createBookCard(createBookObject());
+        body.removeChild(addBox);
+        body.removeChild(overlay);
     })
 })
 
-function addBookToLibrary() {
-
+for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i]);
+    createBookCard(myLibrary[i]);
 }
